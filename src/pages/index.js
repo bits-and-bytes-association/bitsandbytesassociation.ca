d17f8b5ee856888ae7c9013d59c1a92d15e3c02e
Camilo Lucero <6401330+milolucero@users.noreply.github.com>
2023-02-16 13:10:16 -0600

    Add top navigaton component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
 
+// Components
+import TopNavigation from '@/components/TopNavigation';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
@@ -14,6 +17,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+	    <TopNavigation />
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
