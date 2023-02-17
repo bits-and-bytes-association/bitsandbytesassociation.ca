14cb79bf4eba34715306db3ff3b2e001d739d936
Camilo Lucero <6401330+milolucero@users.noreply.github.com>
2023-02-16 18:39:22 -0600

    Refactor links into components

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -2,96 +2,121 @@ import Link from 'next/link';
 import Image from 'next/image';
 import styles from '@/styles/TopNavigation.module.css';
 
-/*
+/**
  * A reusable navigation bar at the top of the page, including items such as logo, links and social media.
  */
 export default function TopNavigation() {
+  /**
+   * Component that generates text links in the top navigation.
+   * @param {string} label  The text that will appear on the link.
+   * @param {string} href   The URL where the user will be redirected when clicking on the link.
+   * @returns A text link.
+   */
+  const TextLink = ({ label, href }) => {
+    return (
+      <li>
+        <Link href={href} className={styles['nav-link']}>
+          {label}
+        </Link>
+      </li>
+    );
+  };
+
+  /**
+   * Component that generates icon links in the top navigation.
+   * @param {string} href  The URL where the user will be redirected when clicking on the link.
+   * @param {string} src  The path where the icon asset is stored.
+   * @param {string} className  The classes to add to the icon.
+   * @param {string} alt  The alternative text of the icon.
+   * @param {int} width  The width of the icon.
+   * @param {int} height  The height of the icon.
+   * @param {bool} priority  Set to true if the image is considered high priority and should preload instead of lazy loading.
+   * @returns An icon link.
+   */
+  const IconLink = ({
+    href,
+    src,
+    className = '',
+    alt = '',
+    width,
+    height,
+    priority = false,
+  }) => {
+    return (
+      <li>
+        <Link href={href}>
+          <Image
+            src={src}
+            alt={alt}
+            className={styles[{ className }]}
+            width={width}
+            height={height}
+            priority={priority}
+          />
+        </Link>
+      </li>
+    );
+  };
+
   return (
     <nav id={styles['top-nav']}>
       <div className={styles['nav-container']}>
-        {/* Main logo */}
-        <Link href="/">
-          <Image
-            src="/images/bba-logo.svg"
-            alt="Bits and Bytes Association logo"
-            className={styles['bba-logo']}
-            width={260}
-            height={80}
-            priority
-          />
-        </Link>
+        {/* BBA main logo */}
+        <IconLink
+          href="/"
+          src="/images/bba-logo.svg"
+          alt="Bits and Bytes Association logo"
+          width={260}
+          height={80}
+          priority={true}
+        />
 
         <div className={styles['nav-links']}>
           {/* Main links */}
           <ul className={styles['nav-text-links']}>
-            <li>
-              <Link href="/about" className={styles['nav-link']}>
-                About
-              </Link>
-            </li>
-            <li>
-              <Link href="/events" className={styles['nav-link']}>
-                Events
-              </Link>
-            </li>
-            <li>
-              <Link href="/volunteer" className={styles['nav-link']}>
-                Volunteer
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact" className={styles['nav-link']}>
-                Contact
-              </Link>
-            </li>
+            <TextLink label="About" href="/about" />
+            <TextLink label="Events" href="/events" />
+            <TextLink label="Volunteer" href="/volunteer" />
+            <TextLink label="Contact" href="/contact" />
           </ul>
 
           {/* Social media icons */}
           <ul className={styles['nav-icon-links']}>
-            <li>
-              <Link href="https://discord.gg/RXySZQE">
-                <Image
-                  src="/images/discord-logomark-black.svg"
-                  alt="Discord logo"
-                  className={styles['discord-logo']}
-                  width={28}
-                  height={28}
-                />
-              </Link>
-            </li>
-            <li>
-              <Link href="https://www.youtube.com/@bitsandbytesassociation7743">
-                <Image
-                  src="/images/youtube-logomark-black.svg"
-                  alt="Youtube logo"
-                  className={styles['youtube-logo']}
-                  width={28}
-                  height={20}
-                />
-              </Link>
-            </li>
-            <li>
-              <Link href="https://www.instagram.com/rrcbba/">
-                <Image
-                  src="/images/instagram-logomark-black.svg"
-                  alt="Instagram logo"
-                  className={styles['instagram-logo']}
-                  width={22}
-                  height={22}
-                />
-              </Link>
-            </li>
-            <li>
-              <Link href="https://www.rrc.ca/explore/computer-and-information-systems-technology/">
-                <Image
-                  src="/images/rrc-logomark-black.svg"
-                  alt="Red River College logo"
-                  className={styles['rrc-logo']}
-                  width={30}
-                  height={30}
-                />
-              </Link>
-            </li>
+            {/* Discord */}
+            <IconLink
+              href="https://discord.gg/RXySZQE"
+              src="/images/discord-logomark-black.svg"
+              alt="Discord logo"
+              width={28}
+              height={28}
+            />
+
+            {/* YouTube */}
+            <IconLink
+              href="https://www.youtube.com/@bitsandbytesassociation7743"
+              src="/images/youtube-logomark-black.svg"
+              alt="Youtube logo"
+              width={28}
+              height={20}
+            />
+
+            {/* Instagram */}
+            <IconLink
+              href="https://www.instagram.com/rrcbba/"
+              src="/images/instagram-logomark-black.svg"
+              alt="Instagram logo"
+              width={22}
+              height={22}
+            />
+
+            {/* RRC */}
+            <IconLink
+              href="https://www.rrc.ca/explore/computer-and-information-systems-technology/"
+              src="/images/rrc-logomark-black.svg"
+              alt="Red River College logo"
+              width={30}
+              height={30}
+            />
           </ul>
         </div>
       </div>
