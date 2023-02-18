0e533ed6551b4ddbb050062f7d96bca57cbac97b
Bradley Anderson <13277940+bandrson@users.noreply.github.com>
2023-02-18 00:35:13 -0600

    Remove last Tailwind breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+const customScreens = Object.assign({}, defaultTheme.screens);
+// Remove last breakpoint
+delete customScreens['2xl'];
+
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: customScreens,
     extend: {},
   },
   plugins: [],
