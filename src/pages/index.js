c06997bb4820dc06d9dd0a848c93f5508d76ad7e
Camilo Lucero <6401330+milolucero@users.noreply.github.com>
2023-02-21 16:37:48 -0600

    Implement color palette (#12)
    * Implement color palette (CSS variables & Tailwind)
    
    * Adjust color references to new palette
    
    * Fix some missed classes
    
    * Add light-font and dark-font Tailwind classes
    
    ---------
    
    Co-authored-by: Bradley Anderson <13277940+bandrson@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,7 @@ export default function Home() {
         {/* Placeholder element */}
         <div className="container mx-auto">
           {/* Sample buttons */}
+          <p className="my-3 text-3xl font-bold">Buttons</p>
           <h5 className="mb-2 text-lg">
             <b>Variant &quot;primary&quot;</b> - Simple link buttons with
             default parameters.
@@ -70,7 +71,7 @@ export default function Home() {
           <h5 className="mb-2 text-lg">
             <b>Variant &quot;dark&quot;</b> - Sample large button with image
           </h5>
-          <div className="mb-5">
+          <div className="mb-10">
             <Button
               variant="dark"
               size="lg"
@@ -87,11 +88,269 @@ export default function Home() {
             </Button>
           </div>
 
+          {/* Color palette */}
+          <p className="my-4 text-3xl font-bold">Color palette</p>
+          <p className="my-2 text-xl font-bold">CSS variables</p>
+          <div className="flex">
+            <div
+              className="mb-10 mr-10 flex flex-col"
+              style={{
+                height: '20em',
+                width: '20em',
+                color: 'var(--light-font-color)',
+              }}
+            >
+              <div
+                style={{ background: 'var(--primary-color)' }}
+                className="flex grow items-center justify-center"
+              >
+                --primary-color
+              </div>
+              <div
+                style={{
+                  background: 'var(--primary-hover-color)',
+                }}
+                className="flex grow items-center justify-center"
+              >
+                --primary-hover-color
+              </div>
+              <div
+                style={{
+                  background: 'var(--light-color)',
+                  color: 'var(--dark-font-color)',
+                }}
+                className="flex grow items-center justify-center"
+              >
+                --light-color
+              </div>
+              <div
+                style={{ background: 'var(--dark-color)' }}
+                className="flex grow items-center justify-center"
+              >
+                --dark-color
+              </div>
+              <div
+                style={{ background: 'var(--dark-hover-color)' }}
+                className="flex grow items-center justify-center"
+              >
+                --dark-hover-color
+              </div>
+            </div>
+
+            <div
+              className="mb-10 mr-10 flex flex-col"
+              style={{
+                height: '20em',
+                width: '20em',
+                color: 'var(--light-font-color)',
+              }}
+            >
+              <div
+                style={{ background: 'var(--primary-color-900)' }}
+                className="flex grow items-center justify-center"
+              >
+                --primary-color-900
+              </div>
+              <div
+                style={{ background: 'var(--primary-color-800)' }}
+                className="flex grow items-center justify-center"
+              >
+                --primary-color-800
+              </div>
+              <div
+                style={{ background: 'var(--primary-color-700)' }}
+                className="flex grow items-center justify-center"
+              >
+                --primary-color-700
+              </div>
+              <div
+                style={{ background: 'var(--primary-color-600)' }}
+                className="flex grow items-center justify-center"
+              >
+                --primary-color-600
+              </div>
+              <div
+                style={{ background: 'var(--primary-color-500)' }}
+                className="flex grow items-center justify-center"
+              >
+                --primary-color-500 (brand)
+              </div>
+              <div
+                style={{ background: 'var(--primary-color-400)' }}
+                className="flex grow items-center justify-center"
+              >
+                --primary-color-400
+              </div>
+              <div
+                style={{ background: 'var(--primary-color-300)' }}
+                className="flex grow items-center justify-center"
+              >
+                --primary-color-300
+              </div>
+              <div
+                style={{ background: 'var(--primary-color-200)' }}
+                className="flex grow items-center justify-center"
+              >
+                --primary-color-200
+              </div>
+              <div
+                style={{ background: 'var(--primary-color-100)' }}
+                className="flex grow items-center justify-center"
+              >
+                --primary-color-100
+              </div>
+              <div
+                style={{ background: 'var(--primary-color-50)' }}
+                className="flex grow items-center justify-center"
+              >
+                --primary-color-50
+              </div>
+            </div>
+
+            <div>
+              <div
+                className="flex flex-col items-center justify-center"
+                style={{
+                  height: '10em',
+                  width: '20em',
+                  background: 'var(--dark-color)',
+                }}
+              >
+                <p
+                  className="text-2xl"
+                  style={{ color: 'var(--light-font-color)' }}
+                >
+                  --light-font-color
+                </p>
+                <p
+                  className="text-2xl"
+                  style={{ color: 'var(--light-font-hover-color)' }}
+                >
+                  --light-font-hover-color
+                </p>
+              </div>
+              <div
+                className="flex flex-col items-center justify-center"
+                style={{
+                  height: '10em',
+                  width: '20em',
+                  background: 'var(--light-color)',
+                }}
+              >
+                <p
+                  className="text-2xl"
+                  style={{ color: 'var(--dark-font-color)' }}
+                >
+                  --dark-font-color
+                </p>
+                <p
+                  className="text-2xl"
+                  style={{ color: 'var(--dark-font-hover-color)' }}
+                >
+                  --dark-font-hover-color
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <p className="my-2 text-xl font-bold">Tailwind classes</p>
+          <div className="flex">
+            <div
+              className="mb-10 mr-10 flex flex-col text-light-font"
+              style={{
+                height: '20em',
+                width: '20em',
+              }}
+            >
+              <div className="flex grow items-center justify-center bg-primary">
+                bg-primary
+              </div>
+              <div className="flex grow items-center justify-center bg-primary-hover">
+                bg-primary-hover
+              </div>
+              <div className="flex grow items-center justify-center bg-light text-dark">
+                bg-light
+              </div>
+              <div className="flex grow items-center justify-center bg-dark">
+                bg-dark
+              </div>
+              <div className="flex grow items-center justify-center bg-dark-hover">
+                bg-dark-hover
+              </div>
+            </div>
+
+            <div
+              className="mb-10 mr-10 flex flex-col text-light-font"
+              style={{
+                height: '20em',
+                width: '20em',
+              }}
+            >
+              <div className="flex grow items-center justify-center bg-primary-900">
+                bg-primary-900
+              </div>
+              <div className="flex grow items-center justify-center bg-primary-800">
+                bg-primary-800
+              </div>
+              <div className="flex grow items-center justify-center bg-primary-700">
+                bg-primary-700
+              </div>
+              <div className="flex grow items-center justify-center bg-primary-600">
+                bg-primary-600
+              </div>
+              <div className="flex grow items-center justify-center bg-primary-500">
+                bg-primary-500 (brand)
+              </div>
+              <div className="flex grow items-center justify-center bg-primary-400">
+                bg-primary-400
+              </div>
+              <div className="flex grow items-center justify-center bg-primary-300">
+                bg-primary-300
+              </div>
+              <div className="flex grow items-center justify-center bg-primary-200">
+                bg-primary-200
+              </div>
+              <div className="flex grow items-center justify-center bg-primary-100">
+                bg-primary-100
+              </div>
+              <div className="flex grow items-center justify-center bg-primary-50">
+                bg-primary-50
+              </div>
+            </div>
+
+            <div>
+              <div
+                className="flex flex-col items-center justify-center bg-dark"
+                style={{
+                  height: '10em',
+                  width: '20em',
+                }}
+              >
+                <p className="text-2xl text-light-font">text-light-font</p>
+                <p className="text-2xl text-light-font-hover">
+                  text-light-font-hover
+                </p>
+              </div>
+              <div
+                className="flex flex-col items-center justify-center bg-light"
+                style={{
+                  height: '10em',
+                  width: '20em',
+                }}
+              >
+                <p className="text-2xl text-dark">text-dark-font</p>
+                <p className="text-2xl text-dark-font-hover">
+                  text-dark-font-hover
+                </p>
+              </div>
+            </div>
+          </div>
+
           {/* Sample Heading component */}
           <div className="my-10">
             <HeadingBBAStyle>Follow us on Instagram</HeadingBBAStyle>
             <HeadingBBAStyle
-              className="text-[2.5rem] font-bold text-sky-500"
+              className="text-[2.5rem] font-bold text-primary"
               inverted={true}
             >
               @rrcbba
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,41 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: customScreens,
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: 'var(--primary-color)',
+          light: 'var(--primary-color-100)',
+          dark: 'var(--primary-color-900)',
+          hover: 'var(--primary-hover-color)',
+          900: 'var(--primary-color-900)',
+          800: 'var(--primary-color-800)',
+          700: 'var(--primary-color-700)',
+          600: 'var(--primary-color-600)',
+          500: 'var(--primary-color-500)',
+          400: 'var(--primary-color-400)',
+          300: 'var(--primary-color-300)',
+          200: 'var(--primary-color-200)',
+          100: 'var(--primary-color-100)',
+          50: 'var(--primary-color-50)',
+        },
+        light: {
+          DEFAULT: 'var(--light-color)',
+        },
+        dark: {
+          DEFAULT: 'var(--dark-color)',
+          hover: 'var(--dark-hover-color)',
+        },
+        'light-font': {
+          DEFAULT: 'var(--light-color)',
+          hover: 'var(--light-font-hover-color)',
+        },
+        'dark-font': {
+          DEFAULT: 'var(--dark-color)',
+          hover: 'var(--dark-font-hover-color)',
+        },
+      },
+    },
   },
   plugins: [],
 };
