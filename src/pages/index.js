572c6a8fed5ccb29070e38a35c82aa59d8baecc3
Bradley Anderson <13277940+bandrson@users.noreply.github.com>
2023-02-21 19:57:16 -0600

    Remove missed parts of the Next.js template

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '@/styles/Home.module.css';
 
 // Components
 import TopNavigation from '@/components/TopNavigation';
