5a7f0a22e625eb40980a2bd80ae4e2701035f0ce
Bradley Anderson <13277940+bandrson@users.noreply.github.com>
2023-03-10 16:52:55 -0600

    Remove premature abstraction from Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,61 +7,48 @@ import Image from 'next/image';
 export default function Footer() {
   return (
     <div className="bg-dark">
-      <Navigation />
-      <BottomNotice />
-    </div>
-  );
-}
-
-/**
- * Footer navigation component
- */
-function Navigation() {
-  return (
-    <nav className="container mx-auto flex py-12 px-16">
-      <NavColumn header="Club">
-        <NavLink href="#">Mission Statement</NavLink>
-        <NavLink href="#">Current Board</NavLink>
-        <NavLink href="#">Past Executives</NavLink>
-      </NavColumn>
-      <NavColumn header="Involvement">
-        <NavLink href="#">Events</NavLink>
-        <NavLink href="#">Volunteer</NavLink>
-        <NavLink href="#">GitHub</NavLink>
-      </NavColumn>
-      <NavColumn header="Socials">
-        <NavLink href="#">Discord</NavLink>
-        <NavLink href="#">Instagram</NavLink>
-        <NavLink href="#">YouTube</NavLink>
-        <NavLink href="#">LinkedIn</NavLink>
-      </NavColumn>
-      <NavColumn header="Get In Touch">
-        <NavLink href="#">Contact</NavLink>
-      </NavColumn>
-      <NavLogo
-        src="/images/bba-logomark-white.svg"
-        alt="Bits and Bytes Association logo"
-      />
-    </nav>
-  );
-}
-
-/**
- * Footer bottom notice component
- */
-function BottomNotice() {
-  return (
-    <div className="border-t border-neutral-800 bg-neutral-900">
-      <p className="cursor-default py-3 text-center text-sm text-gray-400">
-        Made with ❤️ by our{' '}
-        <Link
-          href="#"
-          className="linear underline underline-offset-2 duration-300 hover:text-light-font"
-        >
-          contributors
-        </Link>
-        {'. '}© {new Date().getFullYear()} Bits and Bytes Association
-      </p>
+      <nav className="container mx-auto flex py-12 px-16">
+        <NavColumn header="Club">
+          <NavLink href="#">Mission Statement</NavLink>
+          <NavLink href="#">Current Board</NavLink>
+          <NavLink href="#">Past Executives</NavLink>
+        </NavColumn>
+        <NavColumn header="Involvement">
+          <NavLink href="#">Events</NavLink>
+          <NavLink href="#">Volunteer</NavLink>
+          <NavLink href="#">GitHub</NavLink>
+        </NavColumn>
+        <NavColumn header="Socials">
+          <NavLink href="#">Discord</NavLink>
+          <NavLink href="#">Instagram</NavLink>
+          <NavLink href="#">YouTube</NavLink>
+          <NavLink href="#">LinkedIn</NavLink>
+        </NavColumn>
+        <NavColumn header="Get In Touch">
+          <NavLink href="#">Contact</NavLink>
+        </NavColumn>
+        <div className="ml-auto mr-10 shrink-0 self-center">
+          <Image
+            src="/images/bba-logomark-white.svg"
+            alt="Bits and Bytes Association logo"
+            width={70}
+            height={70}
+            priority
+          />
+        </div>
+      </nav>
+      <div className="border-t border-neutral-800 bg-neutral-900">
+        <p className="cursor-default py-3 text-center text-sm text-gray-400">
+          Made with ❤️ by our{' '}
+          <Link
+            href="#"
+            className="linear underline underline-offset-2 duration-300 hover:text-light-font"
+          >
+            contributors
+          </Link>
+          {'. '}© {new Date().getFullYear()} Bits and Bytes Association
+        </p>
+      </div>
     </div>
   );
 }
@@ -99,18 +86,5 @@ function NavLink({ href, children }) {
         {children}
       </Link>
     </li>
-  );
-}
-
-/**
- * Footer navigation logo component
- * @param {string} src  Image src
- * @param {string} alt  Image alt
- */
-function NavLogo({ src, alt }) {
-  return (
-    <div className="ml-auto mr-10 shrink-0 self-center">
-      <Image src={src} alt={alt} width={70} height={70} priority />
-    </div>
   );
 }
