29672b4967b1ccd04ca839e14e4a22dc88664ca7
Camilo Lucero <6401330+milolucero@users.noreply.github.com>
2023-08-25 11:46:08 -0500

    Implement sticky footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -37,9 +37,13 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <TopNavigation />
-      <Component {...pageProps} />
-      <Footer data={footerLinks} />
+      <div className="globalContainer">
+        <TopNavigation />
+        <div className="pageContainer">
+          <Component {...pageProps} />
+        </div>
+        <Footer data={footerLinks} />
+      </div>
     </>
   );
 }
---
+++ src/pages/past-executives.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function PastExecutivesPage() {
+  return (
+    <>
+      <Head>
+        <title>TEST</title>
+      </Head>
+      <div>Test</div>
+    </>
+  );
+}
