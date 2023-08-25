3a3921f8d54235f8101a7f1671f1c3bad94cf151
Camilo Lucero <6401330+milolucero@users.noreply.github.com>
2023-08-24 20:09:49 -0500

    Separate footer links data into new file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,77 +5,10 @@ import Image from 'next/image';
 const firstYear = 2023;
 const curYear = new Date().getFullYear();
 
-const footerLinks = [
-  {
-    columnTitle: 'Club',
-    links: [
-      {
-        label: 'Mission Statement',
-        href: '#',
-      },
-      {
-        label: 'Current Board',
-        href: '#',
-      },
-      {
-        label: 'Past Executives',
-        href: './past-executives',
-      },
-    ],
-  },
-  {
-    columnTitle: 'Involvement',
-    links: [
-      {
-        label: 'Events',
-        href: '#',
-      },
-      {
-        label: 'Volunteer',
-        href: '#',
-      },
-      {
-        label: 'GitHub',
-        href: 'https://github.com/bits-and-bytes-association/',
-      },
-    ],
-  },
-  {
-    columnTitle: 'Socials',
-    links: [
-      {
-        label: 'Discord',
-        href: 'https://bitsandbytesassociation.ca/assets/img/discord-logo.png',
-      },
-      {
-        label: 'Instagram',
-        href: 'https://www.instagram.com/rrcbba/',
-      },
-      {
-        label: 'YouTube',
-        href: 'https://www.youtube.com/channel/UCQ1KRzD0F7dEuvQs7Brd0YQ',
-      },
-      {
-        label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/bits-and-bytes-association',
-      },
-    ],
-  },
-  {
-    columnTitle: 'Get In Touch',
-    links: [
-      {
-        label: 'Contact',
-        href: '#',
-      },
-    ],
-  },
-];
-
 /**
  * Footer component
  */
-export default function Footer() {
+export default function Footer({ footerLinks }) {
   return (
     <div className="bg-dark">
       <div className="container mx-auto px-6">
