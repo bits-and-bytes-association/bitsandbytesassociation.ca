3ecf0325b775c92440a1d6a4fa7d339244fa88f0
Bradley Anderson <13277940+bandrson@users.noreply.github.com>
2023-10-26 20:55:19 -0500

    Actually fix the hostname in the Next config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'scontent.cdninstagram.com',
+        hostname: '**.cdninstagram.com',
         port: '',
       },
     ],
